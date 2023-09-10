7eabc296960ee3e0f7e4ad2eefc3913b923631b0
Igor Tarasenko <igtarako@gmail.com>
2023-09-10 23:05:17 +0700

    optimistic lock

--- internal/data/models.go
+++ internal/data/models.go
@@ -7,15 +7,18 @@ import (
 
 var (
 	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edt conflict")
 )
 
+type Movies interface {
+	Insert(movie *Movie) error
+	Get(id int64) (*Movie, error)
+	Update(movie *Movie) error
+	Delete(id int64) error
+}
+
 type Models struct {
-	Movies interface {
-		Insert(movie *Movie) error
-		Get(id int64) (*Movie, error)
-		Update(movie *Movie) error
-		Delete(id int64) error
-	}
+	Movies Movies
 }
 
 func NewModels(db *sql.DB) Models {
